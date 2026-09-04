a9c0c387e45c5eb28088ce4e8c15d1a0715c3f71
dixonw2 <mrbacon2015@gmail.com>
2026-09-04 13:01:00 -0400

    Add information and stock image

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,128 @@
-const App = () => {
+import styles from "./App.module.css";
+
+const Greeting = () => {
+  return (
+    <div className={styles.greeting}>
+      <h1>Welcome! I'm Wyatt,</h1>
+      <h3>a software developer with 5 years of experience. </h3>
+    </div>
+  );
+};
+
+const Education = () => {
   return (
     <>
-      <div>
-        <h1>Wyatt Dixon</h1>
-        <p className="">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean et
-          est a dui semper facilisis. Pellentesque placerat elit a nunc. Nullam
-          tortor odio, rutrum quis, egestas ut, posuere sed, felis. Vestibulum
-          placerat feugiat nisl. Suspendisse lacinia, odio non feugiat
-          vestibulum, sem erat blandit metus, ac nonummy magna odio pharetra
-          felis. Vivamus vehicula velit non metus faucibus auctor. Nam sed
-          augue. Donec orci. Cras eget diam et dolor dapibus sollicitudin. In
-          lacinia, tellus vitae laoreet ultrices, lectus ligula dictum dui, eget
-          condimentum velit dui vitae ante. Nulla nonummy augue nec pede.
-          Pellentesque ut nulla. Donec at libero. Pellentesque at nisl ac nisi
-          fermentum viverra. Praesent odio. Phasellus tincidunt diam ut ipsum.
-          Donec eget est.
-        </p>
-      </div>
+      <h4>Education</h4>
+      <p>
+        <b>Northern Kentucky University (2015 - 2019)</b>
+      </p>
+      <p>Bachelor of Science in Computer Science, Minor in Mathematics</p>
     </>
+  );
+};
+
+const Experience = () => {
+  return (
+    <>
+      <h4>Experience</h4>
+      <ul>
+        <li>
+          <b>Software Engineer I</b> at Advantage Solutions (January 2022 -
+          October 2024)
+        </li>
+        <ul>
+          <li>
+            Built and maintained ETL pipelines for clients using Python
+            (Selenium) and .NET/SQL Server.
+          </li>
+          <li>
+            Led the development of an automated config-driven SLA monitoring
+            project that stored metrics from incoming emails in an Excel file
+            daily using Power Automate, read into a DuckDB database with
+            PowerShell, and then evaluated to update a SharePoint list
+            Dashboard.
+          </li>
+        </ul>
+        <li>
+          <b>Software Developer</b> at Invar Systems (May 2020 - August 2021)
+        </li>
+        <ul>
+          <li>
+            Created Warehouse Management software using .NET, WPF, and SQL
+            Server.
+          </li>
+          <li>
+            Traveled to the client's site to perform on-site
+            integration/acceptance/end-to-end testing.
+          </li>
+        </ul>
+        <li>
+          <b>Software Engineer</b> at TATA Consultancy Services (June 2019 - May
+          2020)
+        </li>
+        <ul>
+          <li>
+            Supported a client's website using Java, JSP, JavaScript, Oracle
+            SQL, and SQL Server.
+          </li>
+        </ul>
+      </ul>
+    </>
+  );
+};
+
+const Skills = () => {
+  return (
+    <>
+      <h4>Skills</h4>
+      <ul>
+        <li>
+          <b>Languages:</b> JavaScript, TypeScript, Java, C#, Python
+        </li>
+        <li>
+          <b>Technologies:</b> React, HTML/CSS, RESTful APIs (FastAPI),
+          WPF/Avalonia, Selenium, SQL Server, PostgreSQL, Azure, Git, and some
+          experience with PowerShell and Power Automate
+        </li>
+        <li>
+          <b>Other:</b> Experience in Agile environments (primarily Kanban),
+          Test Driven Development
+        </li>
+      </ul>
+    </>
+  );
+};
+
+const ProfDevelopment = () => {
+  return (
+    <>
+      <h4>Professional Development</h4>
+      <ul>
+        <li>
+          <b>
+            The Ultimate React Course 2025: React, Next.js, Redux & More (Udemy)
+          </b>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <div className={styles.container}>
+      <Greeting />
+      <div className={styles.information}>
+        <Education />
+        <Experience />
+        <Skills />
+        <ProfDevelopment />
+      </div>
+      <img
+        className={styles.side}
+        src="https://thumbs.dreamstime.com/b/businessman-outdoor-business-center-background-successful-business-person-portrait-professional-people-formal-wear-red-119656046.jpg"
+      />
+    </div>
   );
 };
 
